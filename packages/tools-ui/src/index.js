3b9bf16bbbbb33f353aed3a2d43ef994af3fb837
chenkeke <chenkeke@hwunling.com>
2022-10-25 18:03:30 +0800

    整合 本地antd到tools-ui

--- packages/tools-ui/src/index.js
+++ packages/tools-ui/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "cesium/Widgets/widgets.css";
 import "tailwindcss/tailwind.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+import "antd/dist/antd.min.css";
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- packages/tools-ui/src/view/menu/menu.js
+++ packages/tools-ui/src/view/menu/menu.js
@@ -1,11 +1,11 @@
 import "./menu.css";
-// import {Button} from "i-antd";
+import { Button } from "i-antd";
 
 // Language: javascript
 function menu() {
   return (
     <div className={"menu-container"}>
-      {/*<Button type="primary">Primary Button</Button>*/}
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
