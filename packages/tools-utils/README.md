508396b55e9d9aada8f3b5b3887d5bd183a4b6c7
chenkeke <chenkeke@hwunling.com>
2022-10-29 20:18:05 +0800

    创建工具插件

---
+++ packages/tools-utils/README.md
@@ -0,0 +1,11 @@
+# `tools-utils`
+
+> TODO: description
+
+## Usage
+
+```
+const toolsUtils = require('tools-utils');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/tools-utils/__tests__/tools-utils.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const toolsUtils = require("..");
+const assert = require("assert").strict;
+
+assert.strictEqual(toolsUtils(), "Hello from toolsUtils");
+console.info("toolsUtils tests passed");
